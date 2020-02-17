a077bf06f52f0d1e96c831febc51578ca77e2b75
zubaidaakhter <zubaidaakhter345@gmail.com>
2020-02-17 10:20:42 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# labtask1
---
+++ src/javaapplication9/Rectangle.java
@@ -0,0 +1,21 @@
+
+package javaapplication9;
+
+public class Rectangle {
+    
+    double  langth,width;
+     double  area;
+     void setvalue(int langth,int width){
+         this.langth=langth;
+         this.width=width;
+         
+     }
+     void getvalue(){
+         area= langth * width;
+         System.out.println("the area of ractangle "+area);
+     }
+     
+     
+            
+    
+}
